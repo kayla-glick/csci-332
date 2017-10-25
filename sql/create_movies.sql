be19dbc1ce34d645cb44c4434c89542c82ff1f23
kyle-glick <kyleglick99@gmail.com>
2017-10-25 19:25:01 -0400

    Update create_movies.sql

--- sql/create_movies.sql
+++ sql/create_movies.sql
@@ -2,6 +2,7 @@ CREATE TABLE Movies (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT,
+  genre VARCHAR(255) NOT NULL,
   producer VARCHAR(255) NOT NULL,
   release_date TIMESTAMP NOT NULL,
   run_time INT NOT NULL
