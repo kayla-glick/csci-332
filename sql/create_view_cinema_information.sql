956f49b8101cf069b58fa2fe5dd7f0f1091e4828
kyle-glick <kyleglick99@gmail.com>
2017-10-29 12:41:52 -0400

    Update create_view_cinema_information.sql

--- sql/create_view_cinema_information.sql
+++ sql/create_view_cinema_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW CinemaInformation AS
-SELECT cins.id AS cinema_id, cins.name, FullAddress(adds.id) AS address, accs.first_name, accs.last_name, accs.email
+SELECT cins.id AS cinema_id, cins.name, FullAddress(adds.id) AS address, accs.id AS account_id, accs.first_name, accs.last_name, accs.email
 FROM Cinemas cins
 INNER JOIN Addresses adds
 ON adds.id = cins.address_id
