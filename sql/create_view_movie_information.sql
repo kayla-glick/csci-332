e41e8b11ca117c0757f710795a8f417f6e11a1cb
kyle-glick <kyleglick99@gmail.com>
2017-10-29 12:48:41 -0400

    Create create_view_movie_information.sql

---
+++ sql/create_view_movie_information.sql
@@ -0,0 +1,5 @@
+CREATE VIEW MovieInformation AS
+SELECT movs.id AS movie_id, movs.title, movs.description, movs.genre, movs.release_date, movs.run_time, accs.id AS account_id, accs.first_name, accs.last_name, accs.email
+FROM Movies movs
+INNER JOIN Accounts accs
+ON accs.id = movs.producer_id
