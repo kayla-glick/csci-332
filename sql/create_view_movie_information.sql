c628cded2f161b1093f129a40843a63daf96ee23
kyle-glick <kyleglick99@gmail.com>
2017-10-29 16:45:36 -0400

    Update create_view_movie_information.sql

--- sql/create_view_movie_information.sql
+++ sql/create_view_movie_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW MovieInformation AS
-SELECT movs.id AS movie_id, movs.title, movs.description, movs.genre, movs.rating, movs.release_date, movs.run_time, accs.id AS account_id, accs.first_name, accs.last_name, accs.email
+SELECT movs.id AS movie_id, movs.title, movs.description, movs.genre, movs.rating, movs.release_date, movs.run_time, movs.producer_id, accs.first_name, accs.last_name, accs.email
 FROM Movies movs
 INNER JOIN Accounts accs
 ON accs.id = movs.producer_id
