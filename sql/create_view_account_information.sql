4719d3435e04cd460d5e9489d21293312e5f55ca
kyle-glick <kyleglick99@gmail.com>
2017-10-29 15:15:13 -0400

    Update create_view_account_information.sql

--- sql/create_view_account_information.sql
+++ sql/create_view_account_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW AccountInformation AS
-SELECT acct.id AS account_id, acct.first_name, acct.last_name, acct.email, acct.is_owner, acct.is_producer, addr.id AS address_id, FullAddress(addr.id) as address
+SELECT acct.id AS account_id, acct.first_name, acct.last_name, acct.email, acct.is_owner, acct.is_producer, addr.id AS address_id, FullAddress(addr.id) as address, addr.street, addr.city, addr.state, addr.zip
 FROM Accounts acct
 INNER JOIN Addresses addr
 ON acct.address_id = addr.id
