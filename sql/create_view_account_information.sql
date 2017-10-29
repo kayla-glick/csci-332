6366c329a3309d95904eece1560b001eda9974d6
kyle-glick <kyleglick99@gmail.com>
2017-10-29 12:26:29 -0400

    Update create_view_account_information.sql

--- sql/create_view_account_information.sql
+++ sql/create_view_account_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW AccountInformation AS
-SELECT acct.id as account_id, acct.first_name, acct.last_name, acct.email, acct.is_owner, acct.is_producer, addr.id as address_id, addr.street, addr.city, addr.state, addr.zip
-from Accounts acct
+SELECT acct.id AS account_id, acct.first_name, acct.last_name, acct.email, acct.is_owner, acct.is_producer, addr.id AS address_id, addr.street, addr.city, addr.state, addr.zip
+FROM Accounts acct
 INNER JOIN Addresses addr
-on acct.address_id = addr.id
+ON acct.address_id = addr.id
