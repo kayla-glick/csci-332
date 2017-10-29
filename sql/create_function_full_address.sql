4d70a14eca0503559e936794aec17aca63fa43ef
kyle-glick <kyleglick99@gmail.com>
2017-10-29 12:32:09 -0400

    Update create_function_full_address.sql

--- sql/create_function_full_address.sql
+++ sql/create_function_full_address.sql
@@ -1,7 +1,7 @@
 CREATE FUNCTION FullAddress ( address_id INT )
-RETURNS VARCHAR(522)
+RETURNS VARCHAR(524)
 BEGIN
-DECLARE address VARCHAR(522);
-SET address = ( SELECT CONCAT(street, ', ', city, ', ', state, ' ', zip) FROM Addresses WHERE id = address_id );
+DECLARE address VARCHAR(524);
+SET address = ( SELECT CONCAT(street, '<br>', city, ', ', state, ' ', zip) FROM Addresses WHERE id = address_id );
 RETURN address;
 END
