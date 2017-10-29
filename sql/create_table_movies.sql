bf7100ce5a828616a51b1cef72f1ed1b0ea15950
kyle-glick <kyleglick99@gmail.com>
2017-10-29 16:05:53 -0400

    Update create_table_movies.sql

--- sql/create_table_movies.sql
+++ sql/create_table_movies.sql
@@ -3,7 +3,8 @@ CREATE TABLE Movies (
   title VARCHAR(255) NOT NULL,
   description TEXT,
   genre VARCHAR(255) NOT NULL,
+  rating VARCHAR(5) NOT NULL,
   producer_id INT references Accounts(id),
-  release_date TIMESTAMP NOT NULL,
-  run_time INT NOT NULL
+  run_time INT NOT NULL,
+  release_date TIMESTAMP NOT NULL
 );
