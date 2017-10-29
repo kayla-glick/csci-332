7c90c16b2d9e83f0bd4c4ad2e02779babc98ad34
kyle-glick <kyleglick99@gmail.com>
2017-10-29 12:27:40 -0400

    Update and rename create_view_ticket_information to create_view_ticket_information.sql

---
+++ sql/create_view_ticket_information.sql
@@ -0,0 +1,13 @@
+CREATE VIEW TicketInformation AS
+SELECT ticks.id AS ticket_id, ticks.account_id, ticks.purchase_date, shows.show_time, shows.price, thetrs.number as theater_number, cins.name as cinema_name, CONCAT(adds.street, ', ', adds.city, ', ', adds.state, ' ', adds.zip) AS address, movs.title AS movie_title, movs.genre, movs.run_time
+FROM Tickets ticks
+INNER JOIN Showings shows
+ON ticks.showing_id = shows.id
+INNER JOIN Theaters thetrs
+ON shows.theater_id = thetrs.id
+INNER JOIN Cinemas cins
+ON thetrs.cinema_id = cins.id
+INNER JOIN Addresses adds
+ON cins.address_id = adds.id
+INNER JOIN Movies movs
+ON shows.movie_id = movs.id
