76180358c17bad02fb20d93cda6c477d3734a3d2
kyle-glick <kyleglick99@gmail.com>
2017-10-29 12:29:58 -0400

    Update create_view_ticket_information.sql

--- sql/create_view_ticket_information.sql
+++ sql/create_view_ticket_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW TicketInformation AS
-SELECT ticks.id AS ticket_id, ticks.account_id, ticks.purchase_date, shows.show_time, shows.price, thetrs.number as theater_number, cins.name as cinema_name, CONCAT(adds.street, ', ', adds.city, ', ', adds.state, ' ', adds.zip) AS address, movs.title AS movie_title, movs.genre, movs.run_time
+SELECT ticks.id AS ticket_id, ticks.account_id, ticks.purchase_date, shows.show_time, shows.price, thetrs.number as theater_number, cins.name as cinema_name, FullAddress(adds.id) AS address, movs.title AS movie_title, movs.genre, movs.run_time
 FROM Tickets ticks
 INNER JOIN Showings shows
 ON ticks.showing_id = shows.id
