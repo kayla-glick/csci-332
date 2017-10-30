7432d5ab2bc7f767ae0a9f22484a8803ea2740b6
kyle-glick <kyleglick99@gmail.com>
2017-10-29 22:20:38 -0400

    Update create_procedure_default_theaters_proc.sql

--- sql/create_procedure_default_theaters_proc.sql
+++ sql/create_procedure_default_theaters_proc.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE DefaultTheatersProc ( cinema INT, count INT, default_capacity INT )
 BEGIN
 DECLARE i INT;
-SET i = count;
+SET i = 0;
 WHILE i < count DO
 INSERT INTO Theaters (number, capacity, cinema_id) VALUES (i + 1, default_capacity, cinema);
 SET i = i + 1;
