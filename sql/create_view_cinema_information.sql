aaddd675079793d8921c1ef53b117a06b19bc137
kyle-glick <kyleglick99@gmail.com>
2017-11-01 13:59:31 -0400

    Update create_view_cinema_information.sql

--- sql/create_view_cinema_information.sql
+++ sql/create_view_cinema_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW CinemaInformation AS
-SELECT cins.id AS cinema_id, cins.name, FullAddress(adds.id) AS address, adds.id as address_id, adds.street, adds.city, adds.state, adds.zip, cins.owner_id, CONCAT(accs.first_name, ' ', accs.last_name, ' (', accs.email, ')') as owner, ( SELECT COUNT(id) from Theaters where cinema_id = cins.id ) as theater_count, ( SELECT COUNT(id) from Showings where cinema_id = cins.id ) as showing_count
+SELECT cins.id AS cinema_id, cins.name, FullAddress(adds.id) AS address, adds.id as address_id, adds.street, adds.city, adds.state, adds.zip, cins.owner_id, CONCAT(accs.first_name, ' ', accs.last_name, ' (', accs.email, ')') as owner, ( SELECT COUNT(id) from Theaters where cinema_id = cins.id ) as theater_count, ( SELECT COUNT(shows.id) FROM Showings shows INNER JOIN Theaters thets ON shows.theater_id = thets.id WHERE thets.cinema_id = cins.id ) AS showing_count
 FROM Cinemas cins
 INNER JOIN Addresses adds
 ON adds.id = cins.address_id
