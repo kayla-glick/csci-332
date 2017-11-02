1f4d515812ae2c5d714cbce929ceec8debb43831
kyle-glick <kyleglick99@gmail.com>
2017-11-02 16:18:02 -0400

    Update create_view_theater_information.sql

--- sql/create_view_theater_information.sql
+++ sql/create_view_theater_information.sql
@@ -2,4 +2,3 @@ CREATE VIEW TheaterInformation AS
 SELECT thets.id AS theater_id, thets.number, thets.capacity, cins.id AS cinema_id, ( SELECT COUNT(id) FROM Showings WHERE theater_id = thets.id ) as showing_count
 from Theaters thets
 INNER JOIN Cinemas cins ON thets.cinema_id = cins.id
-ORDER BY thets.cinema_id, thets.number
