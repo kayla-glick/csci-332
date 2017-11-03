03219afba7d58a240e1a6cdf881000db87a1905e
kyle-glick <kyleglick99@gmail.com>
2017-11-03 18:21:24 -0400

    Update create_table_theaters.sql

--- sql/create_table_theaters.sql
+++ sql/create_table_theaters.sql
@@ -2,5 +2,6 @@ CREATE TABLE Theaters (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   number INT NOT NULL,
   capacity INT NOT NULL,
-  cinema_id INT NOT NULL REFERENCES Cinemas(id)
+  cinema_id INT NOT NULL REFERENCES Cinemas(id),
+  UNIQUE KEY (number, cinema_id)
 );
