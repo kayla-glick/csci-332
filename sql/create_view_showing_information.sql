37870b0fd4e3c2d8e2605d72682441b52db973a6
kyle-glick <kyleglick99@gmail.com>
2017-11-06 11:19:13 -0500

    Update create_view_showing_information.sql

--- sql/create_view_showing_information.sql
+++ sql/create_view_showing_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW ShowingInformation AS
-SELECT shows.id AS showing_id, shows.show_date, shows.show_time, shows.price, movs.title AS movie, movs.run_time, cins.id AS cinema_id
+SELECT shows.id AS showing_id, shows.show_date, shows.show_time, shows.price, movs.title AS movie, movs.run_time, cins.id AS cinema_id, thets.number as theater_number
 FROM Showings shows
 INNER JOIN Movies movs ON shows.movie_id = movs.id
 INNER JOIN Theaters thets on shows.theater_id = thets.id
