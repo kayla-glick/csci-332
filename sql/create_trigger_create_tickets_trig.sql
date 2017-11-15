f06ab87752176a19c724a0ba9b5bc6ef33e59292
kyle-glick <kyleglick99@gmail.com>
2017-11-14 19:40:56 -0500

    Update create_trigger_create_tickets_trig.sql

--- sql/create_trigger_create_tickets_trig.sql
+++ sql/create_trigger_create_tickets_trig.sql
@@ -5,7 +5,7 @@ BEGIN
 DECLARE i INTEGER;
 SET i = 0;
 WHILE i < NEW.ticket_count DO
-INSERT INTO Tickets (tranaction_id) VALUES (NEW.id);
+INSERT INTO Tickets (transaction_id) VALUES (NEW.id);
 SET i = i + 1;
 END WHILE;
 END
