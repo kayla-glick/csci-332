1728c28d6c108298b59f1dbde2f4b74ca5862391
kyle-glick <kyleglick99@gmail.com>
2017-11-14 19:15:14 -0500

    Update create_table_transactions.sql

--- sql/create_table_transactions.sql
+++ sql/create_table_transactions.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Transactions (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   account_id INT NOT NULL REFERENCES Accounts(id),
+  showing_id INT NOT NULL REFERENCES Showings(id),
   amount INT NOT NULL,
   date TIMESTAMP NOT NULL
 );
