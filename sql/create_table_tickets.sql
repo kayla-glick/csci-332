8684efc9da943ff32f53f7b7dcaa32cf97553d0d
kyle-glick <kyleglick99@gmail.com>
2017-11-14 19:13:57 -0500

    Update create_table_tickets.sql

--- sql/create_table_tickets.sql
+++ sql/create_table_tickets.sql
@@ -1,5 +1,4 @@
 CREATE TABLE Tickets (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  transaction_id INT NOT NULL REFERENCES Transactions(id),
-  showing_id INT NOT NULL REFERENCES Showings(id)
+  transaction_id INT NOT NULL REFERENCES Transactions(id)
 );
