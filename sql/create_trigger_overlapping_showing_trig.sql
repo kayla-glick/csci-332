3eee42ef52101c7ae09174d48777fe46f0ce83fd
kyle-glick <kyleglick99@gmail.com>
2017-11-16 10:49:42 -0500

    Create create_trigger_overlapping_showing_trig.sql

---
+++ sql/create_trigger_overlapping_showing_trig.sql
@@ -0,0 +1,23 @@
+CREATE TRIGGER OverlapShowingTrig
+BEFORE INSERT ON Showings
+FOR EACH ROW
+BEGIN
+DECLARE overlapping INTEGER;
+DECLARE run_time INTEGER;
+DECLARE end_time TIME;
+SET run_time = ( SELECT run_time FROM Movies WHERE id=NEW.movie_id );
+SET end_time = AddTime(NEW.show_time, SEC_TO_TIME(run_time * 60));
+SET overlapping = ( 
+    SELECT COUNT(*) FROM Showings shows
+    INNER JOIN Movies movs ON movs.id=shows.movie_id
+    WHERE shows.theater_id=NEW.theater_id AND shows.show_date=NEW.show_date
+    AND (
+        (NEW.show_time >= shows.show_time AND NEW.show_time <= AddTime(shows.show_time, SEC_TO_TIME(movs.run_time * 60)))
+        OR
+        (end_time >= shows.show_time AND end_time <= AddTime(shows.show_time, SEC_TO_TIME(movs.run_time * 60)))
+    )
+);
+IF overlapping > 0 THEN
+SIGNAL sqlstate '45001' SET message_text="There is already a showing in this theater at this time.";
+END IF;
+END
