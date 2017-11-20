7ff672ff4cdba0ecb5535ea623ab8a3c819b7b42
kyle-glick <kyleglick99@gmail.com>
2017-11-20 17:21:32 -0500

    Update create_table_theaters.sql

--- sql/create_table_theaters.sql
+++ sql/create_table_theaters.sql
@@ -2,6 +2,7 @@ CREATE TABLE Theaters (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   number INT NOT NULL,
   capacity INT NOT NULL,
-  cinema_id INT NOT NULL REFERENCES Cinemas(id) ON DELETE CASCADE,
-  UNIQUE KEY (number, cinema_id)
+  cinema_id INT NOT NULL,
+  UNIQUE KEY (number, cinema_id),
+  FOREIGN KEY (cinema_id) REFERENCES Cinemas(id) ON DELETE CASCADE
 );
