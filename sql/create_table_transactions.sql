f49e78027c62c132b628f22650c970f3ed253c03
kyle-glick <kyleglick99@gmail.com>
2017-11-20 17:22:53 -0500

    Update create_table_transactions.sql

--- sql/create_table_transactions.sql
+++ sql/create_table_transactions.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Transactions (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  account_id INT NOT NULL REFERENCES Accounts(id) ON DELETE CASCADE,
-  showing_id INT NOT NULL REFERENCES Showings(id) ON DELETE CASCADE,
+  account_id INT NOT NULL,
+  showing_id INT NOT NULL,
   ticket_count INT NOT NULL,
   amount INT NOT NULL,
-  date TIMESTAMP NOT NULL
+  date TIMESTAMP NOT NULL,
+  FOREIGN KEY (account_id) REFERENCES Accounts(id) ON DELETE CASCADE,
+  FOREIGN KEY (showing_id) REFERENCES Showings(id) ON DELETE CASCADE
 );
