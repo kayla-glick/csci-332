02d8fb47731db1f8b0b14e32609f255dfcbbacda
kyle-glick <kyleglick99@gmail.com>
2017-11-20 17:55:25 -0500

    Update create_view_movie_information.sql

--- sql/create_view_movie_information.sql
+++ sql/create_view_movie_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW MovieInformation AS
-SELECT DISTINCT movs.id AS movie_id, movs.title, movs.description, movs.genre, movs.rating, movs.release_date, movs.run_time, movs.producer_id, CONCAT(accs.first_name, ' ', accs.last_name) AS producer
+SELECT movs.id AS movie_id, movs.title, movs.description, movs.genre, movs.rating, movs.release_date, movs.run_time, movs.producer_id, CONCAT(accs.first_name, ' ', accs.last_name) AS producer
 FROM Movies movs
 INNER JOIN Accounts accs
 ON accs.id = movs.producer_id
