ad0840783adb3b6a7bc1f857d2ff7dfa09c6a249
kyle-glick <kyleglick99@gmail.com>
2017-11-27 11:06:48 -0500

    Update create_view_showings_report.sql

--- sql/create_view_showings_report.sql
+++ sql/create_view_showings_report.sql
@@ -1,8 +1,7 @@
 CREATE VIEW ShowingsReport AS
-SELECT cins.id AS cinema_id, thets.number AS theater_number, thets.capacity, shows.show_date, shows.show_time, shows.price, movs.title AS movie, AddTime(shows.show_time, SEC_TO_TIME(movs.run_time * 60)) AS end_time, COUNT(ticks.id) AS ticket_count
+SELECT cins.id AS cinema_id, thets.number AS theater_number, thets.capacity, shows.show_date, shows.show_time, shows.price, movs.title AS movie, AddTime(shows.show_time, SEC_TO_TIME(movs.run_time * 60)) AS end_time, (SELECT COUNT(*) FROM Tickets ticks WHERE ticks.transaction_id=trans.id) AS ticket_count
 FROM Cinemas cins
-INNER JOIN Theaters thets ON cins.id=thets.cinema_id
-INNER JOIN Showings shows ON thets.id=shows.theater_id
-INNER JOIN Movies movs ON shows.movie_id=movs.id
-INNER JOIN Transactions trans ON shows.id=trans.showing_id
-INNER JOIN Tickets ticks on trans.id=ticks.transaction_id
+LEFT OUTER JOIN Theaters thets ON cins.id=thets.cinema_id
+LEFT OUTER JOIN Showings shows ON thets.id=shows.theater_id
+LEFT OUTER JOIN Movies movs ON shows.movie_id=movs.id
+LEFT OUTER JOIN Transactions trans ON shows.id=trans.showing_id
